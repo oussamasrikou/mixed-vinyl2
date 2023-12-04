c3b7b5d571eace51b68fff52cf1bd1b5bfbbddad
= <=>
2023-12-04 18:29:39 +0100

    The Query Builder

--- src/Repository/VinylMixRepository.php
+++ src/Repository/VinylMixRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\VinylMix;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -35,7 +36,24 @@ class VinylMixRepository extends ServiceEntityRepository
 //            ->getResult()
 //        ;
 //    }
-
+public function findAllOrderedByVotes(string $genre = null): array
+    {
+        $queryBuilder = $this->addOrderByVotesQueryBuilder();
+        
+        if ($genre) {
+            $queryBuilder->andWhere('mix.genre = :genre')
+                ->setParameter('genre', $genre);
+        }
+        return $queryBuilder
+        ->getQuery()
+        ->getResult()
+    ;
+   }
+private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
+{
+    $queryBuilder = $queryBuilder ?? $this->createQueryBuilder('mix');
+    return $queryBuilder->orderBy('mix.votes', 'DESC');
+}
 //    public function findOneBySomeField($value): ?VinylMix
 //    {
 //        return $this->createQueryBuilder('v')
