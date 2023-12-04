bede0658947aa61a2e3940a0f95ce9d35a617715
= <=>
2023-12-04 18:17:47 +0100

    Custom Entity Methods & Twig Magic

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -36,7 +36,7 @@ class VinylController extends AbstractController
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
        
-        $mixes = $mixRepository->findAll();
+        $mixes = $mixRepository->findBy([], ['votes' => 'DESC']);
         
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
