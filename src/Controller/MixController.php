62f2634c812ca1db31ec70a83d73f0770158928b
= <=>
2023-12-04 18:59:30 +0100

    The Request Object

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class MixController extends AbstractController
 {
@@ -36,5 +37,16 @@ class MixController extends AbstractController
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        dd($mix);
     }
 }
