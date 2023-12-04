330578cfc883098f4e1518b18aa3e5c745c64e0e
= <=>
2023-12-04 18:09:02 +0100

    Querying the Database

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\VinylMix;
+use App\Service\MixRepository;
+use App\Repository\VinylMixRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use function Symfony\Component\String\u;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,11 +31,16 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
+
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+       
+        $mixes = $mixRepository->findAll();
+        
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
     }
 }
